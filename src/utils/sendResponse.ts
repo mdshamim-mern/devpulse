5ff75b84be8b09dc658b923f5457b107e55ee95f
Md Shamim <mdshamim.mern@gmail.com>
2026-05-22 08:50:30 +0600

    Documented response utility function

--- src/utils/sendResponse.ts
+++ src/utils/sendResponse.ts
@@ -1,3 +1,4 @@
+// Utility function for consistent API responses
 import { Response } from 'express';
 
 const sendResponse = <T>(res: Response, data: { statusCode: number; success: boolean; message: string; data?: T }) => {
